cdc4ea83d6028d477d3c7b6e27327fd79ebea3b1
cami2178 <camila200178@gmail.com>
2024-10-03 23:43:11 -0300

    meGusta

---
+++ index.js
@@ -0,0 +1,18 @@
+var numero = 0
+function darLike(likes){
+    numero++;
+    document.getElementById(likes).innerHTML=numero;
+}
+
+var number = 0
+function darLike3(likes){
+    number++;
+    document.getElementById(likes).innerHTML=number;
+}
+
+var numero2 = 0
+function darLike2(likes){
+    numero2++;
+    document.getElementById(likes).innerHTML=numero2;
+}
+
